3a95cdc329f9bc38e350e0d351aa1c6425cb20f2
Yan Carvalhal <yan.carvalhal@somagrupo.com.br>
2025-09-10 14:50:18 -0300

    ajustando imagens e links

--- src/components/ProejctImages.tsx
+++ src/components/ProejctImages.tsx
@@ -1,8 +1,43 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
+import image1 from "@/assets/carousel/project-1.jpeg";
+import image2 from "@/assets/carousel/project-2.jpeg";
+import image3 from "@/assets/carousel/project-3.jpeg";
+import image4 from "@/assets/carousel/project-4.jpeg";
+import image5 from "@/assets/carousel/project-5.jpeg";
+import image6 from "@/assets/carousel/project-6.jpeg";
+import image7 from "@/assets/carousel/project-7.jpeg";
+import image8 from "@/assets/carousel/project-8.jpeg";
+import image9 from "@/assets/carousel/project-9.jpeg";
+import image10 from "@/assets/carousel/project-10.jpeg";
+import image11 from "@/assets/carousel/project-11.jpeg";
+import image12 from "@/assets/carousel/project-12.jpeg";
+import image13 from "@/assets/carousel/project-13.jpeg";
+import image14 from "@/assets/carousel/project-14.jpeg";
+import image15 from "@/assets/carousel/project-15.jpeg";
+import image16 from "@/assets/carousel/project-16.jpeg";
+import image17 from "@/assets/carousel/project-17.jpeg";
+import image18 from "@/assets/carousel/project-18.jpeg";
 
 const ProjectImages = () => {
   const images = [
-    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18
   ]
 
   return (
@@ -11,9 +46,9 @@ const ProjectImages = () => {
         <CarouselPrevious/>
 
         <CarouselContent>
-          {images.map((number) => (
+          {images.map((image) => (
             <CarouselItem>
-              <img className="max-h-[500px]" src={`project-${number}.jpeg`} alt="" />
+              <img className="max-h-[500px]" src={image} alt="" />
             </CarouselItem>
           ))}
         </CarouselContent>
